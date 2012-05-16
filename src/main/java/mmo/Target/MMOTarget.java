6ac7403401d8bffe67bf337dd0b4f1e2d87559b5
Wulfspider <hello@wulfspider.com>
2012-05-16 11:41:33 -0400

    Updated pom.xml, readme, plugin.yml, and added header.txt. Added automatic license header checking and formatting. Formatted headers to be project specific. A little bit more formatting on misc files.

--- src/main/java/mmo/Target/MMOTarget.java
+++ src/main/java/mmo/Target/MMOTarget.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ * This file is part of mmoTarget <http://github.com/mmoMinecraftDev/mmoTarget>.
  *
- * mmoMinecraft is free software: you can redistribute it and/or modify
+ * mmoTarget is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
